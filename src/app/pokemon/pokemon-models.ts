19b88290b22d16c488b08e63e64904bfcb0af7be
tomasEtchecopar <tomasEtchecopar2203@gmail.com>
2025-10-25 21:14:45 -0300

    tipado de pokemon

--- src/app/pokemon/pokemon-models.ts
+++ src/app/pokemon/pokemon-models.ts
@@ -11,3 +11,74 @@ export interface NamedAPIResourceList {
     previous: string | null;         // Previous page URL
     results: NamedAPIResource[];     // Current page items
 }
+
+
+
+export interface Pokemon {
+    id: number;
+    name: string;
+    height: number; // decimetres
+    weight: number; // hectograms
+    base_experience: number;
+
+    abilities: PokemonAbility[];
+    forms: NamedAPIResource[];
+    game_indices: PokemonGameIndex[];
+    held_items: PokemonHeldItem[];
+    location_area_encounters: string;
+    sprites: PokemonSprites;
+    species: NamedAPIResource;
+    stats: PokemonStat[];
+    types: PokemonType[];
+}
+
+export interface NamedAPIResource {
+    name: string;
+    url: string;
+}
+
+export interface PokemonAbility {
+    ability: NamedAPIResource;
+    is_hidden: boolean;
+    slot: number;
+}
+
+export interface PokemonGameIndex {
+    game_index: number;
+    version: NamedAPIResource;
+}
+
+export interface PokemonHeldItem {
+    item: NamedAPIResource;
+    version_details: {
+        rarity: number;
+        version: NamedAPIResource;
+    }[];
+}
+
+export interface PokemonMove {
+    move: NamedAPIResource;
+    version_group_details: {
+        level_learned_at: number;
+        move_learn_method: NamedAPIResource;
+        version_group: NamedAPIResource;
+    }[];
+}
+
+export interface PokemonSprites {
+    front_default: string | null;
+    back_default: string | null;
+    front_shiny: string | null;
+    back_shiny: string | null
+}
+
+export interface PokemonStat {
+    base_stat: number;
+    effort: number;
+    stat: NamedAPIResource;
+}
+
+export interface PokemonType {
+    slot: number;
+    type: NamedAPIResource;
+}
