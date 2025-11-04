3665f25f76fc2ed6377f13cfd2b3d443d4d155cc
tomasEtchecopar <tomasEtchecopar2203@gmail.com>
2025-11-04 20:34:41 -0300

    creation of ui component for filters

---
+++ src/app/pokemon/pokemon-filter-dropdown/pokemon-filter-dropdown.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pokemon-filter-dropdown',
+  imports: [],
+  templateUrl: './pokemon-filter-dropdown.html',
+  styleUrl: './pokemon-filter-dropdown.css',
+})
+export class PokemonFilterDropdown {
+
+}
