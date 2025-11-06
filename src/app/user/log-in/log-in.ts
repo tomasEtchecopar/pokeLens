18f7f8ac9aea417fa28d4d61750630645ecb26d3
Adrian Gonzalez <adriannggonzalez@gmail.com>
2025-11-06 20:12:54 -0300

    soy mogolico

--- src/app/user/log-in/log-in.ts
+++ src/app/user/log-in/log-in.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { AuthServ } from './auth-serv';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-log-in',
@@ -7,5 +10,28 @@ import { Component } from '@angular/core';
   styleUrl: './log-in.css',
 })
 export class LogIn {
-  
+
+  private readonly auth = inject(AuthServ);
+  private readonly fb = inject(FormBuilder);
+  private readonly router = inject(Router);
+
+  protected readonly form = this.fb.nonNullable.group({
+    username: ['', Validators.required],
+    password: ['', Validators.required]
+  });
+
+  logIn() {
+    if (this.form.invalid) {
+      alert('Complete correctamente todos los campos');
+      return;
+    }
+
+    const { username, password } = this.form.getRawValue();
+    this.auth.login(username, password)
+
+    if (this.auth.isLoggedIn()) {
+      this.router.navigate(['/catalog']);
+    }
+  }
+
 }
