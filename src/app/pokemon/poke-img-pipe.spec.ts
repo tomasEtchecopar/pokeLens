817136dbf81fc3bd5e063da37517e049f19112c8
tomasEtchecopar <tomasEtchecopar2203@gmail.com>
2025-11-07 01:27:30 -0300

    creo q ahi esta

--- src/app/pokemon/poke-img-pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { PokeImgPipe } from './poke-img-pipe';
-
-describe('PokeImgPipe', () => {
-  it('create an instance', () => {
-    const pipe = new PokeImgPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
