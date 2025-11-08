9766944456fe872713508c2b810339b73933304c
tomasEtchecopar <tomasEtchecopar2203@gmail.com>
2025-11-07 21:04:08 -0300

    pokelist removed from app.ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,7 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from "./header/header";
-import { effect } from '@angular/core';
-import { inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { PokemonListService } from './pokemon/pokemon-list-service';
 
 @Component({
   selector: 'app-root',
@@ -14,7 +11,5 @@ import { PokemonListService } from './pokemon/pokemon-list-service';
 })
 export class App {
   protected readonly title = signal('pokeLens');
-  //injecting service to load all pokemons in memory
-  private readonly pokemonListService = inject(PokemonListService);
   
 }
