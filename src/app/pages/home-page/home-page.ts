515df936ac20c6f5d6f3ce1e1a2620661b17836e
Sebastian-Farias <juansebastianfarias09@gmail.com>
2025-11-11 15:40:36 -0300

    Minor changes

--- src/app/pages/home-page/home-page.ts
+++ src/app/pages/home-page/home-page.ts
@@ -6,12 +6,12 @@ import { PokemonCard } from "../../pokemon/pokemon-card/pokemon-card";
 
 @Component({
   selector: 'app-home-page',
-  imports: [PokemonCatalog, PokemonCard],
+  imports: [PokemonCard],
   templateUrl: './home-page.html',
   styleUrl: './home-page.css',
 })
 export class HomePage {
-  private readonly dailyPokemonService = inject(DailyPokemonService); 
+  private readonly dailyPokemonService = inject(DailyPokemonService);
 
   readonly pokemonOfTheDay = this.dailyPokemonService.pokemonOfTheDay;
 }
