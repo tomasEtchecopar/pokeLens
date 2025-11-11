5085c563421d7b87529c8696fc36f3b795121b5e
Sebastian-Farias <juansebastianfarias09@gmail.com>
2025-11-11 17:40:44 -0300

    Renovated user-client service
    Added more functionality, on top of making the functions less prone to errors

--- src/app/core/user-client.service.ts
+++ src/app/core/user-client.service.ts
@@ -1,6 +1,8 @@
+import { Observable, switchMap } from 'rxjs';
 import { User } from '../user/user-model';
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
+import { pokemonVault } from '../pages/pokemon-collections/collection-model';
 
 
 
@@ -13,12 +15,28 @@ export class UserClient {
   private readonly http = inject(HttpClient);
   private readonly baseUrl = "http://localhost:3000/users"
 
-  addUser(user: User){
+  getUserById(id: string): Observable<User>{
+    return this.http.get<User>(`${this.baseUrl}/${id}`);
+  }
+
+  addUser(user: User): Observable<User>{
     return this.http.post<User>(this.baseUrl, user);
   }
 
-  updateUser(user: User, id: string | number){
+  updateUser(user: User, id: string | number): Observable<User>{
     return this.http.put<User>(`${this.baseUrl}/${id}`, user)
   }
+
+  addPokemonToVault(userId: string, nuevoPokemon: pokemonVault): Observable<User> {
+  return this.getUserById(userId).pipe(
+    switchMap(usuario => {
+      const vault = usuario.pokemonVault ?? [];
+      const nextId = vault.length ? Math.max(...vault.map(p => p.arrayId)) + 1 : 1;
+      const updatedVault = [...vault, { ...nuevoPokemon, arrayId: nextId }];
+      return this.http.patch<User>(`${this.baseUrl}/${userId}`, { pokemonVault: updatedVault });
+    })
+  );
+}
+
 
 }
