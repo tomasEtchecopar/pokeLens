7f4f48efa64eec1f9e0923338376d1d520bce786
tomasEtchecopar <tomasEtchecopar2203@gmail.com>
2025-11-20 14:19:00 -0300

    fix 2

--- src/enviroments/enviroment.ts
+++ src/enviroments/enviroment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://pokelens-backend.onrender.com/' // Lo llenaremos después
+  apiUrl: 'https://pokelens-backend.onrender.com' // Lo llenaremos después
 };
