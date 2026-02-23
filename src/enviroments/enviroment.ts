8f5dc025ae77cec7fa776db8223409b19daeaa68
Adrián González <adriannggonzalez@gmail.com>
2026-02-23 09:54:41 -0300

    Fix leftover conflict markers

--- src/enviroments/enviroment.ts
+++ src/enviroments/enviroment.ts
@@ -1,10 +1,5 @@
 export const environment = {
   production: true,
-<<<<<<< HEAD
           apiUrl: 'https://pokelens-backend.onrender.com/api'
-          //  apiUrl:'http://localhost:3000/api'
-=======
-         //  apiUrl: 'https://pokelens-backend.onrender.com/api'
-           apiUrl:'http://localhost:3000/api'
->>>>>>> 7a249da (Se creo el componente edit-profile, separando responsabilidades del form del signin, y fixearon vulnerabilidades del front)
+        //    apiUrl:'http://localhost:3000/api'
  };
